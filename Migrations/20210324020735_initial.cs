b0310e0c935ed26c9e04ca80fb3decf235c49128
idayley <71055111+idayley@users.noreply.github.com>
2021-03-24 20:18:18 -0600

    Add project files.

---
+++ Migrations/20210324020735_initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment9.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Edited = table.Column<bool>(nullable: false),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
